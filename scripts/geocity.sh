5184f6555097da2039cf57c50108963de560ec26
Jose L. Marcos <jose-louis.marcos@cnes.fr>
2024-10-29 16:27:44 +0100

    Now properly showing text for wmo weather codes and City Namewq

---
+++ scripts/geocity.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# This script is to get name location from lat/lon coordinates present in the weather.json file
+# so that conky will display it correctly.
+# Google api is used
+
+lat=`cat ~/.cache/weather.json | jq -r '.latitude'`
+lon=`cat ~/.cache/weather.json | jq -r '.longitude'`
+
+# this is the url to retrieve forecast data. Please refer to https://open-meteo.com/en/docs 
+# to view all availables parameters.
+#url="http://api.openweathermap.org/geo/1.0/reverse?lat={lat}&lon={lon}"
+url="https://api-bdc.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lon}"
+
+curl ${url} -s 
